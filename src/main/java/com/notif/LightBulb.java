e923dd6a24afcfdb452535239f5f86148be6d907
Abdul <asalam@aconex.com>
2015-12-21 15:11:43 +0530

    Implement OFF to ON transition

--- src/main/java/com/notif/LightBulb.java
+++ src/main/java/com/notif/LightBulb.java
@@ -2,7 +2,14 @@ package com.notif;
 
 
 public class LightBulb {
+    private boolean off = true;
+
     public boolean is_off() {
-        return true;
+        return this.off;
+    }
+
+    public LightBulb on() {
+        this.off = false;
+        return this;
     }
 }
--- src/test/java/com/notif/LightBulbTest.java
+++ src/test/java/com/notif/LightBulbTest.java
@@ -1,15 +1,29 @@
 package com.notif;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
 import static org.junit.Assert.assertThat;
 
 public class LightBulbTest {
 
+    private LightBulb lightBulb;
+
+    @Before
+    public void setUp() throws Exception {
+        lightBulb = new LightBulb();
+    }
+
     @Test
     public void a_new_light_is_always_off() {
-        assertThat(new LightBulb().is_off(), is(true));
+        assertThat(lightBulb.is_off(), is(true));
+    }
+
+    @Test
+    public void an_off_light_can_be_turned_on(){
+        assertThat(lightBulb.on().is_off(), is(false));
     }
 
 }
